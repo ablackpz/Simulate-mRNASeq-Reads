9bbc84df73216c4cb8fb756e09ebe7622812dd5a
Alexis Black Pyrkosz <ablackpz@msu.edu>
2012-07-27 14:19:50 -0400

    Cleanup old scripts

--- makeActualReadNum.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-fileHandle='DataSet'
-
-for var in {88..88}
-do
-    printf "%d\n" "$var"
-#    awk '{print $8}' $fileHandle$var'Reads.txt' > $fileHandle$var'Reads.txtActual'
-    python remove-method-readtxt.py $fileHandle$var'Reads.txt' $fileHandle$var'.txtActual'
-done
--- makeIndices.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-fileHandle='DataSet'
-
-for var in {131..131}
-do
-    printf "%d\n" "$var"
-    ~/Downloads/bowtie-0.12.0-beta1/./bowtie-build $fileHandle$var'.fa' $fileHandle$var'Index'
-done
